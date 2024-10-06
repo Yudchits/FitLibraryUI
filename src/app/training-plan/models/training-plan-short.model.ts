de513213e69bd30a15c63b6b82d9cd8ff180dad7
Yudchits <pashka.yudchits@gmail.com>
2024-10-06 18:14:48 +0300

    added 'training plan short' model

---
+++ src/app/training-plan/models/training-plan-short.model.ts
@@ -0,0 +1,9 @@
+export class TrainingPlanShort {
+    public Id: number;
+    public Name: string;
+    public Description: string;
+    public Sport: string;
+    public Price: number;
+    public Rating: number;
+    public CreatorId: number;
+}
