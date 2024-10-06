3c10f53b45b46c2aef12fd8b7a8a5c9240bcbc89
Yudchits <pashka.yudchits@gmail.com>
2024-10-06 16:44:50 +0300

    added skeleton of 'TrainingPlanMain' component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TrainingPlanModuleComponent } from './training-plan/training-plan-module/training-plan-module.component';
+import { TrainingPlanMainComponent } from './training-plan/training-plan-main/training-plan-main.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'training-plan', component: TrainingPlanModuleComponent, children: [
+    { path: '', component: TrainingPlanMainComponent }
+  ]}
 ];
 
 @NgModule({
---
+++ src/app/training-plan/training-plan-main/training-plan-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-training-plan-main',
+  templateUrl: './training-plan-main.component.html',
+  styleUrls: ['./training-plan-main.component.css']
+})
+export class TrainingPlanMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
