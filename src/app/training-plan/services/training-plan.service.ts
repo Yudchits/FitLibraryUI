2d02f5172f67a61570abec558a99e69ffc2242e0
Yudchits <pashka.yudchits@gmail.com>
2024-10-07 17:29:05 +0300

    added 'getTrainingPlanById' method in 'TrainingPlanService'

--- src/app/training-plan/services/training-plan.service.ts
+++ src/app/training-plan/services/training-plan.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { TrainingPlanShort } from '../models/training-plan-short.model';
+import { TrainingPlanFull } from '../models/training-plan-full.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +12,19 @@ export class TrainingPlanService {
 
   private readonly trainingPlanApi: string;
   private readonly getAllUrl: string;
+  private readonly getByIdUrl: string;
 
   constructor(private http: HttpClient) { 
     this.trainingPlanApi = environment.url + '/api/trainingPlan';
     this.getAllUrl = '/getAllTrainingPlans';
+    this.getByIdUrl = '/getTrainingPlanById?id=';
   }
 
   getAllTrainingPlans(): Observable<TrainingPlanShort[]> {
     return this.http.get<TrainingPlanShort[]>(this.trainingPlanApi + this.getAllUrl);
+  }
+
+  getTrainingPlanById(id: number): Observable<TrainingPlanFull> {
+    return this.http.get<TrainingPlanFull>(this.trainingPlanApi + this.getByIdUrl + id);
   }
 }
