435c0b00274fb263c4468002504ceb66faddcde4
Yudchits <pashka.yudchits@gmail.com>
2024-10-07 17:22:56 +0300

    added 'exercise' model

---
+++ src/app/training-plan/models/exercise.model.ts
@@ -0,0 +1,10 @@
+export class Exercise {
+    public id: number;
+    public week: number;
+    public exerciseName: string;
+    public sets: number;
+    public repetitions: number;
+    public weight: number;
+    public time: string;
+    public restPeriod: string;
+}
