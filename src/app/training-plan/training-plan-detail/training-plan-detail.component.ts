f5a4d85e0771b645b32d7173a59c8416a4fc0ee0
Yudchits <pashka.yudchits@gmail.com>
2024-10-13 11:16:40 +0300

    added merging of table cells with equal week

--- src/app/training-plan/training-plan-detail/training-plan-detail.component.ts
+++ src/app/training-plan/training-plan-detail/training-plan-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TrainingPlanService } from '../services/training-plan.service';
 import { TrainingPlanFull } from '../models/training-plan-full.model';
@@ -14,8 +14,12 @@ export class TrainingPlanDetailComponent implements OnInit {
 
   trainingPlan: TrainingPlanFull = null;
   isPlanLoaded: boolean = false;
+  @ViewChildren('weekTd') weekTds: QueryList<ElementRef>;
 
-  constructor(private route: ActivatedRoute, private trainingPlanService: TrainingPlanService) { 
+  constructor(private route: ActivatedRoute, 
+    private trainingPlanService: TrainingPlanService, 
+    private changeDetector: ChangeDetectorRef
+  ) { 
   }
 
   ngOnInit(): void {
@@ -30,8 +34,36 @@ export class TrainingPlanDetailComponent implements OnInit {
     this.isPlanLoaded = false;
     this.trainingPlanService.getTrainingPlanById(id).subscribe((trainingPlan: TrainingPlanFull) => {
       this.trainingPlan = trainingPlan || null;
+      this.trainingPlan.exercises = this.trainingPlan.exercises.sort((a, b) => a.week - b.week);
       this.isPlanLoaded = true;
+      this.changeDetector.detectChanges();
+      this.mergeWeekRows();
     });
+  }
+
+  private mergeWeekRows(): void {
+    const weekTds = this.weekTds.toArray();
+
+    let currentWeek = null;
+    let currentWeekId = null;
+
+    const exercises = this.trainingPlan.exercises;
+    const exercisesLength = exercises.length;
+
+    for (let i = 0; i < exercisesLength; i++) {
+      if (exercises[i].week != currentWeek) {
+        currentWeek = exercises[i].week;
+        currentWeekId = i;
+        
+        weekTds[currentWeekId].nativeElement.setAttribute('rowspan', 1);
+      } else {
+        const mergedTd = weekTds[currentWeekId];
+        const mergedTdRowspan = parseInt(mergedTd.nativeElement.getAttribute('rowspan'));
+        mergedTd.nativeElement.setAttribute('rowspan', mergedTdRowspan + 1)
+  
+        weekTds[i].nativeElement.style = 'display: none';
+      }
+    }
   }
 
 }
