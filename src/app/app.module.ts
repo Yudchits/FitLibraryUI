8b54f96c4568225370d87d3a8ce4c25b3a3fc641
Yudchits <pashka.yudchits@gmail.com>
2024-10-19 09:33:05 +0300

    added truncate pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { WeekdayPipe } from './training-plan/common/pipes/weekday.pipe';
 import { TrainingPlanCreateComponent } from './training-plan/training-plan-create/training-plan-create.component';
 import { TextareaAutosizeDirective } from './training-plan/common/directives/textarea-autosize.directive';
 import { TableEditContextMenuComponent } from './shared/table-edit-context-menu/table-edit-context-menu.component';
+import { TruncatePipe } from './training-plan/common/pipes/truncate.pipe';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { TableEditContextMenuComponent } from './shared/table-edit-context-menu/
     WeekdayPipe,
     TrainingPlanCreateComponent,
     TextareaAutosizeDirective,
-    TableEditContextMenuComponent
+    TableEditContextMenuComponent,
+    TruncatePipe
   ],
   imports: [
     BrowserModule,
